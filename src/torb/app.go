d3927337bb7ece18066c1e834b981d4dfc7a4827
ShotaKitazawa <skitazawa1121@gmail.com>
2019-09-04 01:39:39 +0900

    キャッシュからランダムにシートIDを取得するようにした

--- src/torb/app.go
+++ src/torb/app.go
@@ -747,9 +747,8 @@ func main() {
 		var reservationID int64
 		sheet_id := randomSheetCache[event.ID-1][params.Rank][0]
 		for {
-
 			// if err := db.QueryRow("SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1", event.ID, params.Rank).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
-			if err := db.QueryRow("SELECT * FROM sheets WHERE sheet_id = ? AND id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE)", sheet_id, event.ID).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
+			if err := db.QueryRow("SELECT * FROM sheets WHERE id = ? AND id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE)", sheet_id, event.ID).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
 				if err == sql.ErrNoRows {
 					return resError(c, "sold_out", 409)
 				}
